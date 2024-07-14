4dc3908051606a567883c67454c7eaa0aa93dc73
James Harris <jameseharrisii@gmail.com>
2024-07-14 16:03:32 -0700

    Adding Piping CSV files || ignore comments || data response is working

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # NodeJs_Developer_proj
+* Working on Planets from Keplear Data
+* Using CSV large data files
+* attempting ambition project || not quite sure what i'm doing || but what the heck! 
--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+// Working Witth CSV-Parse files in NODE
+// Trying to pulling in Kepler_Data from CSV
+
+
 const { parse } = require('csv-parse');
 const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
+
+const results = [];
 
 
-parse()
+fs.createReadStream('kepler_data.csv')
+// Piping CSV files from LEFT to RIGHT in a readable FORMAT
+// Ignore comment # || ***provide readable data back in COLUMNS || 
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        results.push(data);
+
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });
+
+// parse()
